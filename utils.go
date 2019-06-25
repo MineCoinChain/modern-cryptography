c5d51b1e109130e2f84a721e47b5fd4421ad79d8
MineCoinChain <1105541797@qq.com>
2019-06-25 12:01:39 +0800

    读取公私钥文件转换为

---
+++ utils.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"crypto/rsa"
+	"io/ioutil"
+	"encoding/pem"
+	"crypto/x509"
+	"github.com/pkg/errors"
+)
+//读取公钥文件转换为公钥
+func readRSAPublicKey(filename string) (*rsa.PublicKey, error) {
+	//读取公钥文件
+	info, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	//解码,得到block
+	block, _ := pem.Decode(info)
+	//得到der字符串
+	der := block.Bytes
+	//得到公钥
+	pubInterface, err := x509.ParsePKIXPublicKey(der)
+	if err != nil {
+		return nil, err
+	}
+	pubKey, ok := pubInterface.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("Generate PublicKey failed")
+	}
+	return pubKey, nil
+}
+//读取私钥文件转换为私钥
+func readRSAPriKey(filename string) (*rsa.PrivateKey, error) {
+	//读取私钥文件
+	info, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	//解码，得到block
+	block, _ := pem.Decode(info)
+	//得到der
+	der := block.Bytes
+
+	//得到私钥
+	priKeyInter, err := x509.ParsePKCS8PrivateKey(der)
+	if err != nil {
+		return nil, err
+	}
+	priKey, ok := priKeyInter.(*rsa.PrivateKey)
+	if !ok {
+		return nil, errors.New("priKey no ok!")
+	}
+	return priKey, nil
+}
